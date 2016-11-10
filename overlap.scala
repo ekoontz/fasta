8d5742ba6d69c7861f9732d9e7284a7dd18efd6b
Eugene Koontz <ekoontz@hiro-tan.org>
2016-11-10 08:41:09 -0800

    able to print file contents, one line at a time

--- overlap.scala
+++ overlap.scala
@@ -1,8 +1,26 @@
 object overlap {
+  def usage() {
+    println("usage: overlap <input file>")
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-    val foo = "GCGCCCGGGGCAAGAGTCATTATACTTGAGAATATACATTTAACAGCGGGCTCATAGCAC"
-    println(foo)
+    if (args.length != 1) {
+      usage()
+      System.exit(1)
+    }
+    println("reading from input file:" + args(0));
+    val contents = scala.io.Source.fromFile(args(0)).mkString
+    println(" length of file" + contents.length)
+
+    // split into strings
+    val lines = contents.split("\n")
+
+    val it = lines.iterator
+
+    while(it.hasNext) {
+      println("line: " + it.next())
+    }
+
   }
 }
 
