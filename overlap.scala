dbf9b301a3416c9b462366b234a0633d76a7fa51
Eugene Koontz <ekoontz@hiro-tan.org>
2016-11-10 07:39:34 -0800

    initial commit

---
+++ overlap.scala
@@ -0,0 +1,11 @@
+object overlap {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+    val foo = "GCGCCCGGGGCAAGAGTCATTATACTTGAGAATATACATTTAACAGCGGGCTCATAGCAC"
+    println(foo)
+  }
+}
+
+
+
+
