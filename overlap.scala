3a7ad059ac4f93880fc16cb9c3518ec7ec8e5f07
Eugene Koontz <ekoontz@hiro-tan.org>
2016-11-10 15:56:32 -0800

    prints out entire non-overlapping sequence

--- overlap.scala
+++ overlap.scala
@@ -9,6 +9,7 @@ object overlap {
     // - i < a.length
     // - a[i,a.length] == b[0,a.length]
     //
+    // If none, return 0
     for(i <- 1 to (a.length / 2)) {
       if (a.slice(i,a.length) == b.slice(0,a.length - i)) {
         return i
@@ -23,32 +24,74 @@ object overlap {
       System.exit(1)
     }
 
-    val label2read = read_input(args(0))
+    val reads = read_input(args(0))
+    val lefts = reads.keysIterator
 
-    val lefts = label2read.keysIterator
+    var prefix = Map[String,String]()
+    var prefix_at = Map[String,Integer]()
+    var prefix_label = Map[String,String]()
 
     while(lefts.hasNext) {
-      var first_label = lefts.next()
-      val rights = label2read.keysIterator
+      var first_label = lefts.next
+      val a = reads(first_label)
+      val rights = reads.keysIterator
 
       while(rights.hasNext) {
-        val second_label = rights.next()
-        val a = label2read(first_label)
-        val b = label2read(second_label)
+        val second_label = rights.next
+        val b = reads(second_label)
 
-        val second_as_b =
-          a_then_b(a,b)
+        val at = a_then_b(a,b)
+        val show_this_many_chars = 10
 
-        if (second_as_b > 0) {
-          println(first_label + "@" + second_as_b + " is a prefix of " +
-            second_label + ":" + 
-            a.slice(second_as_b,second_as_b + 5) + " ... " +
-            a.slice(a.length - 5,a.length) + " == " +
-            b.slice(0,5) + " ... " +
-            b.slice(a.length - second_as_b - 5,
-              (a.length - second_as_b)))
+        if (at > 0) {
+          println(first_label + "@" + at + " is a prefix of " +
+            second_label + ": " + 
+            a.slice(at,at + show_this_many_chars) + "..." +
+            a.slice(a.length - show_this_many_chars,a.length) + " == " +
+            b.slice(0,show_this_many_chars) + "..." +
+            b.slice(a.length - at - show_this_many_chars,
+              (a.length - at)))
+          prefix = prefix + (a -> b)
+          prefix_at = prefix_at + (first_label -> at)
+          prefix_label = prefix_label + (first_label -> second_label)
         }
       }
+    }
+
+    // find the first of the reads: the one which is not a
+    // right side (a value of any key in prefixes)
+    var first_in_string = prefix_label.keys.toSet
+    var prefix_label_i = prefix_label.keys.iterator
+
+    while(prefix_label_i.hasNext) {
+      val left_label = prefix_label_i.next
+      val right_label = prefix_label(left_label)
+      first_in_string = first_in_string - right_label
+    }
+
+    if (first_in_string.size == 1) {
+      var current_label = first_in_string.iterator.next
+      while (current_label != null) {
+        print(current_label)
+        current_label = prefix_label.getOrElse(current_label,null)
+        if (current_label != null) {
+          print(" -> ")
+        }
+      }
+      println("")
+      current_label = first_in_string.iterator.next
+      while (current_label != null) {
+        val left = reads(current_label)
+        val at = prefix_at.get(current_label)
+        if (at == None) {
+          print(left + "\n")
+        } else {
+          print(left.slice(0,at.get))
+        }
+        current_label = prefix_label.getOrElse(current_label,null)
+      }
+    } else {
+      println(" Error: no unique first read.")
     }
   }
 
@@ -65,23 +108,23 @@ object overlap {
 
     val it = lines.iterator
     val delimiter_pattern = """^>(.*)""".r
-    var label2read = Map[String,String]()
+    var reads = Map[String,String]()
     var current_label = ""
 
     while(it.hasNext) {
       var line = it.next()
       line match {
         case delimiter_pattern(label) => {
-          label2read = label2read + (label -> "")
+          reads = reads + (label -> "")
           current_label = label
         }
         case _ => {
-          label2read = label2read + (current_label -> (label2read(current_label) + line))
+          reads = reads + (current_label -> (reads(current_label) + line))
         }
       }
     }
 
-    return label2read
+    return reads
 
   }
 
